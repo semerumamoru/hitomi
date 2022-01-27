2566fbafd20e1d914e8f92aa5f9d093b7e6ff844
Semeru Mamoru <semerumamoru@gmail.com>
2022-01-27 14:25:07 +0900

    first commit

---
+++ src/hitomi.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+# use package list
+#use Daemon;
+
+##############
+# main section
+##############
+#
+# auth section
+
+# select target section
+
+# set params section
+
+# 
